c5ec10083b1f252b3065608fff962a1d1196aa79
Isadora Farache <farachedev@gmail.com>
2024-08-13 20:29:41 -0300

    nova-versao

---
+++ readme.md
@@ -0,0 +1,23 @@
+# Site para o Abrigo Mascotinho
+
+Junto com minha vontade colocar em pratica os meus conhecimentos e ajudar uma causa social.
+Tive a ideia de desenvolver esse site em prol de um abrigo que ajuda os animais.
+
+[🔗 Clique aqui para acessar](https://abrigomascotinho.github.io/site/)
+
+
+## 🛠 Tecnologias
+
+- HTML
+- CSS
+- JavaScript
+
+## 🖤 Contato
+
+farachedev@gmail.com
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,19 @@
+const imgs = document.getElementById("imagens");
+const imagens = document.querySelectorAll("#imagens img");
+
+let idx = 0;
+
+function carrossel() {
+    idx++;
+
+    // Reinicie o índice se ele exceder o número de imagens
+    if (idx >= imagens.length) {
+        idx = 0;
+    }
+
+    // Atualize a transformação para o próximo slide
+    imgs.style.transform = `translateX(${-idx * 100}%)`;
+}
+
+// Altere o intervalo conforme necessário
+setInterval(carrossel, 2000);
