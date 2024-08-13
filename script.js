f2f19bacb88e19d50b1809500b87c3ccd9ee1501
Isadora Farache <farachedev@gmail.com>
2024-08-13 20:45:14 -0300

    nova-versao

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ let idx = 0;
 
 function carrossel() {
     idx++;
-
-<<<<<<< HEAD
     // Reinicie o índice se ele exceder o número de imagens
     if (idx >= imagens.length) {
         idx = 0;
@@ -18,14 +16,3 @@ function carrossel() {
 
 // Altere o intervalo conforme necessário
 setInterval(carrossel, 2000);
-=======
-    if (idx > imagens.length - 1) {
-        idx = 0;
-    }
-
-    imgs.style.transform = `translateX(${-idx * 200}px)`;
-
-}
-
-setInterval(carrossel, 1800);
->>>>>>> 816dfc6e5b6f0f5550bc289e11c7a7563dcc8194
